ecd0c11b201b04e7129a0c6845cee3e7078d6441
maxinelobato <maxinelobato89@hotmail.com>
2023-11-11 18:57:06 -0300

    changes

--- src/app/components/CardHelp/index.tsx
+++ src/app/components/CardHelp/index.tsx
@@ -5,12 +5,10 @@ export default function CardHelp() {
     <section className="bg-white">
       <div className="mx-auto max-w-screen-xl px-4 py-12 sm:px-6 md:py-16 lg:px-8">
         <div className="mx-auto max-w-3xl text-center">
-          {/* <h2 className="text-3xl font-bold text-gray-900 sm:text-4xl">
-            Tenha a ajuda necessária com uma Especialista em Direito de Família
-          </h2> */}
           <h1 className="py-4 mb-5 font-sans font-semibold text-4xl tracking-tight text-cyan-950 sm:text-5xl sm:leading-none">
-          Tenha a ajuda necessária de uma Especialista em <span className="font-extrabold">Direito de Família</span>
-              </h1>
+            Tenha a ajuda necessária de uma Especialista em{" "}
+            <span className="font-extrabold">Direito de Família</span>
+          </h1>
           <div className="mt-8 sm:mt-12">
             <ButtonCardHelp />
           </div>
--- src/app/components/GoogleBusiness/index.tsx
+++ src/app/components/GoogleBusiness/index.tsx
@@ -1,64 +1,49 @@
+"use client";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/navigation";
+
+import { Navigation } from "swiper/modules";
+import Image from "next/image";
+
 export default function GoogleBusiness() {
   return (
     <>
       <section className="bg-gray-50">
-        <div className="mx-auto max-w-[1340px] px-4 py-12 sm:px-6 lg:me-0 lg:py-16 lg:pe-0 lg:ps-8 xl:py-24">
-          <div className="max-w-7xl items-end justify-between sm:flex sm:pe-6 lg:pe-8">
-            <h2 className="max-w-xl text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
-              Read trusted reviews from our customers
-            </h2>
-
-            <div className="mt-8 flex gap-4 lg:mt-0">
-              <button
-                aria-label="Previous slide"
-                id="keen-slider-previous"
-                className="rounded-full border border-rose-600 p-3 text-rose-600 transition hover:bg-rose-600 hover:text-white"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="h-5 w-5 rtl:rotate-180"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M15.75 19.5L8.25 12l7.5-7.5"
-                  />
-                </svg>
-              </button>
-
-              <button
-                aria-label="Next slide"
-                id="keen-slider-next"
-                className="rounded-full border border-rose-600 p-3 text-rose-600 transition hover:bg-rose-600 hover:text-white"
-              >
-                <svg
-                  className="h-5 w-5 rtl:rotate-180"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M9 5l7 7-7 7"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                  />
-                </svg>
-              </button>
+        <div className="mx-auto max-w-7xl px-4 py-12 sm:px-6">
+          <div className="mx-auto max-w-screen-xl">
+            <div className="mx-auto max-w-3xl text-center">
+              <h1 className="font-sans font-semibold text-4xl tracking-tight text-cyan-950 sm:text-5xl sm:leading-none">
+                O que os clientes falam sobre a{" "}
+                <span className="font-extrabold">
+                  Lenoir Advogados Associados
+                </span>
+              </h1>
+            </div>
+            <div className="mt-10 flex flex-wrap items-center justify-center gap-4 xl:justify-center">
+              <Image
+                src="/google.png"
+                alt="Imagem"
+                width={300}
+                height={300}
+                loading="lazy"
+                className="w-28 h-28 drop-shadow-lg"
+              />
             </div>
           </div>
 
           <div className="-mx-6 mt-8 lg:col-span-2 lg:mx-0">
-            <div id="keen-slider" className="keen-slider">
-              <div className="keen-slider__slide">
-                <blockquote className="flex h-full flex-col justify-between bg-white p-6 shadow-sm sm:p-8 lg:p-12">
+            <Swiper
+              navigation={true}
+              modules={[Navigation]}
+              className="mySwiper"
+            >
+              <SwiperSlide>
+                <blockquote className="flex h-full flex-col justify-center bg-white p-6 shadow-sm sm:p-8 lg:p-12">
                   <div>
-                    <div className="flex gap-0.5 text-green-500">
+                    <div className="flex gap-0.5 text-cyan-950">
                       <svg
                         className="h-5 w-5"
                         fill="currentColor"
@@ -106,30 +91,26 @@ export default function GoogleBusiness() {
                     </div>
 
                     <div className="mt-4">
-                      <p className="text-2xl font-bold text-rose-600 sm:text-3xl">
-                        Stayin Alive
+                      <p className="text-2xl font-bold text-cyan-800 sm:text-3xl">
+                        Cliente 1
                       </p>
 
-                      <p className="mt-4 leading-relaxed text-gray-700">
-                        No, Rose, they are not breathing. And they have no arms
-                        or legs … Where are they? You know what? If we come
-                        across somebody with no arms or legs, do we bother
-                        resuscitating them? I mean, what quality of life do we
-                        have there?
+                      <p className="mt-4 leading-relaxed font-sans text-lg text-cyan-950">
+                        Depoimento Cliente 1
                       </p>
                     </div>
                   </div>
 
-                  <footer className="mt-4 text-sm font-medium text-gray-700 sm:mt-6">
-                    &mdash; Michael Scott
+                  <footer className="mt-4 text-sm font-medium text-cyan-600 sm:mt-6">
+                    &mdash; Client 1
                   </footer>
                 </blockquote>
-              </div>
+              </SwiperSlide>
 
-              <div className="keen-slider__slide">
-                <blockquote className="flex h-full flex-col justify-between bg-white p-6 shadow-sm sm:p-8 lg:p-12">
+              <SwiperSlide>
+                <blockquote className="flex h-full flex-col justify-center bg-white p-6 shadow-sm sm:p-8 lg:p-12">
                   <div>
-                    <div className="flex gap-0.5 text-green-500">
+                    <div className="flex gap-0.5 text-cyan-950">
                       <svg
                         className="h-5 w-5"
                         fill="currentColor"
@@ -177,30 +158,26 @@ export default function GoogleBusiness() {
                     </div>
 
                     <div className="mt-4">
-                      <p className="text-2xl font-bold text-rose-600 sm:text-3xl">
-                        Stayin Alive
+                      <p className="text-2xl font-bold text-cyan-800 sm:text-3xl">
+                        Cliente 2
                       </p>
 
-                      <p className="mt-4 leading-relaxed text-gray-700">
-                        No, Rose, they are not breathing. And they have no arms
-                        or legs … Where are they? You know what? If we come
-                        across somebody with no arms or legs, do we bother
-                        resuscitating them? I mean, what quality of life do we
-                        have there?
+                      <p className="mt-4 leading-relaxed font-sans text-lg text-cyan-950">
+                        Depoimento Cliente 2
                       </p>
                     </div>
                   </div>
 
-                  <footer className="mt-4 text-sm font-medium text-gray-700 sm:mt-6">
-                    &mdash; Michael Scott
+                  <footer className="mt-4 text-sm font-medium text-cyan-600 sm:mt-6">
+                    &mdash; Cliente 2
                   </footer>
                 </blockquote>
-              </div>
+              </SwiperSlide>
 
-              <div className="keen-slider__slide">
-                <blockquote className="flex h-full flex-col justify-between bg-white p-6 shadow-sm sm:p-8 lg:p-12">
+              <SwiperSlide>
+                <blockquote className="flex h-full flex-col justify-center bg-white p-6 shadow-sm sm:p-8 lg:p-12">
                   <div>
-                    <div className="flex gap-0.5 text-green-500">
+                    <div className="flex gap-0.5 text-cyan-950">
                       <svg
                         className="h-5 w-5"
                         fill="currentColor"
@@ -248,26 +225,22 @@ export default function GoogleBusiness() {
                     </div>
 
                     <div className="mt-4">
-                      <p className="text-2xl font-bold text-rose-600 sm:text-3xl">
-                        Stayin Alive
+                      <p className="text-2xl font-bold text-cyan-800 sm:text-3xl">
+                        Cliente 3
                       </p>
 
-                      <p className="mt-4 leading-relaxed text-gray-700">
-                        No, Rose, they are not breathing. And they have no arms
-                        or legs … Where are they? You know what? If we come
-                        across somebody with no arms or legs, do we bother
-                        resuscitating them? I mean, what quality of life do we
-                        have there?
+                      <p className="mt-4 leading-relaxed font-sans text-lg text-cyan-950">
+                        Depoimento Cliente 3
                       </p>
                     </div>
                   </div>
 
-                  <footer className="mt-4 text-sm font-medium text-gray-700 sm:mt-6">
-                    &mdash; Michael Scott
+                  <footer className="mt-4 text-sm font-medium text-cyan-600 sm:mt-6">
+                    &mdash; Cliente 3
                   </footer>
                 </blockquote>
-              </div>
-            </div>
+              </SwiperSlide>
+            </Swiper>
           </div>
         </div>
       </section>
