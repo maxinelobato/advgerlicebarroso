3191a76c9adc8f6df626f7d386fdf4a5e0df5cba
maxinelobato <maxinelobato89@hotmail.com>
2023-11-17 16:48:51 -0300

    changes

--- src/app/components/CardHelp/index.tsx
+++ src/app/components/CardHelp/index.tsx
@@ -5,17 +5,17 @@ export default function CardHelp() {
   return (
     <section className="bg-white">
       <div className="mx-auto max-w-screen-xl px-4 py-12 sm:px-6 md:py-16 lg:px-8">
-      <div className="mx-auto max-w-3xl text-center">
+      <div className="mx-auto max-w-4xl text-center">
           <h1 className="py-4 mb-5 font-sans font-semibold text-4xl tracking-tight text-zinc-950 sm:text-5xl sm:leading-none">
-            Por que contratar a Dra. Gerlice da{" "}
-            <span className="font-extrabold">Lenoir Advocacia?</span>
+            Por que contratar a {" "}
+            <span className="font-extrabold">Dra. Gerlice Barroso?</span>
           </h1>
         </div>
         <WhyLenoir />
-        <div className="mx-auto max-w-3xl text-center">
+        <div className="mx-auto max-w-4xl text-center">
           <h1 className="py-4 mb-5 font-sans font-semibold text-4xl tracking-tight text-zinc-950 sm:text-5xl sm:leading-none">
-            Tenha a ajuda necessária de uma Especialista em{" "}
-            <span className="font-extrabold">Direito de Família e Sucessões</span>
+            Tenha a ajuda necessária de uma {" "}
+            <span className="font-extrabold">Especialista em Direito de Família e Sucessões</span>
           </h1>
           <div className="mt-8 sm:mt-12">
             <ButtonCardHelp />
