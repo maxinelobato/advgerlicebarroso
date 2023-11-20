96fcae2ef747d14b4aaa7d2af60c11824796e8cc
maxinelobato <maxinelobato89@hotmail.com>
2023-11-20 20:53:23 -0300

    changes

--- src/app/components/HowWeHelp/index.tsx
+++ src/app/components/HowWeHelp/index.tsx
@@ -89,7 +89,7 @@ export default function HowWeHelp() {
           <path d="M164.47,195.63a8,8,0,0,1-6.7,12.37H10.23a8,8,0,0,1-6.7-12.37,95.83,95.83,0,0,1,47.22-37.71,60,60,0,1,1,66.5,0A95.83,95.83,0,0,1,164.47,195.63Zm87.91-.15a95.87,95.87,0,0,0-47.13-37.56A60,60,0,0,0,144.7,54.59a4,4,0,0,0-1.33,6A75.83,75.83,0,0,1,147,150.53a4,4,0,0,0,1.07,5.53,112.32,112.32,0,0,1,29.85,30.83,23.92,23.92,0,0,1,3.65,16.47,4,4,0,0,0,3.95,4.64h60.3a8,8,0,0,0,7.73-5.93A8.22,8.22,0,0,0,252.38,195.48Z"></path>
         </svg>
       ),
-      title: "Planejtamento Sucessório",
+      title: "Planejamento Sucessório",
       desc: "Um ato de amor e responsabilidade que assegura que seus desejos sejam respeitados, evita conflitos familiares e protege seus entes queridos de encargos desnecessários.",
     },
   ];
@@ -116,7 +116,7 @@ export default function HowWeHelp() {
                     <div className="text-white">{item.icon}</div>
                   </div>
 
-                  <h1 className="p-4 text-center text-lg font-extrabold text-white">
+                  <h1 className="p-4 text-center text-lg uppercase font-extrabold text-white">
                     {item.title}
                   </h1>
                   <p className="text-center text-sm text-white">{item.desc}</p>
