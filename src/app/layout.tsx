3e5b976a4f168dc571039854b93e2b49e010ca41
maxinelobato <maxinelobato89@hotmail.com>
2023-11-20 20:57:13 -0300

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
-import { Suspense } from "react";
+import GoogleTagManager from "@magicul/next-google-tag-manager";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,33 +23,31 @@ export default function RootLayout({
         name="facebook-domain-verification"
         content="0pp7qrk8a62xbewr980vkh2xrevbua"
       /> */}
-      <Suspense>
-        <Script
-          id="gtag-base"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
+
+      {/* <Script
+        id="gtag-base"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer', 'GTM-59RLMBM2');
           `,
-          }}
-        ></Script>
-      </Suspense>
+        }}
+      ></Script> */}
       <body className={inter.className}>
-        <Suspense>{children}</Suspense>
-        <Suspense>
-          <noscript>
-            <iframe
-              src={`https://www.googletagmanager.com/ns.html?id=GTM-59RLMBM2`}
-              height="0"
-              width="0"
-              style={{ display: "none", visibility: "hidden" }}
-            />
-          </noscript>
-        </Suspense>
+        <GoogleTagManager id="GTM-59RLMBM2" />
+        {children}
+        {/* <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=GTM-59RLMBM2`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript> */}
       </body>
     </html>
   );
